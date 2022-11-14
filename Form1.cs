ffac381394a12bdb594d1256be772132088e552a
student <student@LAB-PAR-2>
2022-11-14 16:25:51 +0100

    Dodaj pliki projektów.

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+namespace Cygi5
+{
+    public partial class Form1 : Form
+    {
+        string sekwencja;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = sekwencja;
+        }
+    }
+}
